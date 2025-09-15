21e40321fd26cefc2f8724a352edfc9ab847b671
Ali <aaliyev14291@ada.edu.az>
2025-09-15 21:21:02 +0400

    GC

---
+++ src/lesson11/Book.java
@@ -0,0 +1,15 @@
+package lesson11;
+
+public class Book {
+    String title;
+
+    public Book(String title) {
+        this.title = title;
+        System.out.println("The book named " + title + " has been created");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("The book named " + title + " has been deleted");
+    }
+}
---
+++ src/lesson11/LibraryManager.java
@@ -0,0 +1,18 @@
+package lesson11;
+
+public class LibraryManager {
+    public static void main(String[] args) throws InterruptedException {
+        Book b1 = new Book("Cinayet ve Ceza");
+        Book b2 = new Book("Ali ve Nino");
+        Book b3 = new Book("Sherlock Holmes");
+
+        b1 = null;
+        b2 = null;
+        b3 = null;
+
+        System.gc();
+        Thread.sleep(3000);
+        System.out.println("Thread pause");
+    }
+
+}
