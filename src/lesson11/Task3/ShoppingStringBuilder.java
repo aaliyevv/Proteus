c4f826d6d2ac0c6c847996405b8b26580f2e2475
Ali <aaliyev14291@ada.edu.az>
2025-09-17 00:14:00 +0400

    Garbage Collector

--- src/lesson11/Task3/ShoppingStringBuilder.java
+++ src/lesson11/Task3/ShoppingStringBuilder.java
@@ -2,28 +2,21 @@ package lesson11.Task3;
 
 public class ShoppingStringBuilder {
         public static void main(String[] args) {
-            // Məhsullar massivi
             String[] items = {"Çörək", "Süd", "Yumurta", "Pendir", "Yağ"};
 
-            // StringBuilder yaradılır
             StringBuilder sb = new StringBuilder();
 
-            // Məhsulları siyahıya əlavə edirik
             for (int i = 0; i < items.length; i++) {
                 sb.append(items[i]); // məhsulu əlavə et
 
-                // sonuncudan başqa məhsulların arasına ", " qoy
                 if (i < items.length - 1) {
                     sb.append(", ");
                 }
             }
 
-            // Sonda nida işarəsi əlavə et
             sb.append("!");
 
-            // Nəticəni çap et
             System.out.println(sb.toString());
         }
     }
 
-}
---
+++ src/lesson11/Task3/WordCombination.java
@@ -0,0 +1,23 @@
+package lesson11.Task3;
+
+public class WordCombination {
+    public static void main(String[] args) {
+
+
+        String[] words = {"Java", "StringBuilder", "ilə", "asan", "və", "sürətlidir"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            sb.append(words[i]);
+
+            if (i < words.length - 1) {
+                sb.append(" ");
+            }
+        }
+
+        sb.append(".");
+        System.out.println(sb);
+
+    }
+}
