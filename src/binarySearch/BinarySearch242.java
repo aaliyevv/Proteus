c111208f20e4a6612fa655be2c4928dabe9bca23
Ali <aaliyev14291@ada.edu.az>
2025-09-20 21:38:43 +0400

    Anagram

--- src/binarySearch/BinarySearch242.java
+++ src/binarySearch/BinarySearch242.java
@@ -1,4 +1,26 @@
 package binarySearch;
 
+import java.util.Arrays;
+
 public class BinarySearch242 {
+
+    public static boolean isAnagram (String s, String t){
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char [] sArr = s.toCharArray();
+        char [] tArr = t.toCharArray();
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        return Arrays.equals(sArr, tArr);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram("rat", "car"));
+    }
 }
