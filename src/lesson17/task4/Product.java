35ae2443f7be08c1bb32c9ae25a92da588c4943f
Ali <aaliyev14291@ada.edu.az>
2025-10-12 23:46:22 +0400

    Product

---
+++ src/lesson17/task4/Product.java
@@ -0,0 +1,44 @@
+package lesson17.task4;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Product {
+    String name;
+    String category;
+    double price;
+    int stock;
+
+    public Product(String name, String category, double price, int stock) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                '}';
+    }
+}
